d1f80b4aa42b0cc4f0652a67b8dfbfadbd0daaf4
paullogoncalves <paullogoncalves@gmail.com>
2021-06-16 10:42:44 -0300

    initial commit

---
+++ src/test/java/br/com/oragentalent5/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oragentalent5.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
